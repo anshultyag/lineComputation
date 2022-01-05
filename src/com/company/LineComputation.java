5780f4265d72e5c8e721b58864684463881a85e5
anshultyag <87016992+anshultyag@users.noreply.github.com>
2022-01-05 23:25:33 +0530

    Update LineComputation.java

--- src/com/company/LineComputation.java
+++ src/com/company/LineComputation.java
@@ -26,14 +26,5 @@ public class LineComputation {
         Integer x = Integer.valueOf(val1);
         Integer y = Integer.valueOf(val2);
         System.out.println("lines are equal :"+ x.equals(y));
-
-        int result = x.compareTo(y);
-        if (result > 0 )
-            System.out.println("Line1 is greater than line2");
-        else if(result < 0 )
-            System.out.println("line2 is greater than line1");
-        else
-            System.out.println("line 1 is greater than line 2");
-
     }
 }
